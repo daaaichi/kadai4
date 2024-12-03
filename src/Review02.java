11155fcf36c4c12de56c427f989884d6cd240912
daaaichi <ne11002457if5gf@gmail.com>
2024-12-03 22:24:39 +0900

    課題修正

--- src/Review02.java
+++ src/Review02.java
@@ -2,11 +2,15 @@ public class Review02 {
 
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
-            if (i % 5 == 0 || i % 10 == 5 || i / 10 == 5) {
-                System.out.println("Buzz");
-            } else if (i % 3 == 0 || i % 10 == 3 || i / 10 == 3) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0 ) {
 
                 System.out.println("Fizz");
+            } else if
+
+                (i % 5 == 0 ) {
+                    System.out.println("Buzz");
             }  else {
                 System.out.println(i);
 
